c2250b6a0646c9336f3325a9c21daee04ff8a595
aslam-enkaizen <aslam.hossain@enkaizen.com>
2022-07-06 21:17:36 +0600

    Initial commit

---
+++ src/main/java/com/exrade/DbMigrateApplication.java
@@ -0,0 +1,13 @@
+package com.exrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMigrateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbMigrateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exrade/DbMigrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMigrateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
